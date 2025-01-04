61e68be16f0119cb873d241a36da4630794363ca
mrtkarakis <mertkaraks@icloud.com>
2025-01-04 16:13:30 +0300

    Edit main.dart

---
+++ lib/my_app.dart
@@ -0,0 +1,10 @@
+part of 'main.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
